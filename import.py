9954cc62ca441f2ff6316d2c80f4c7d7d900e837
chienfuchen32 <chienfuchen32@gmail.com>
2019-02-04 01:06:19 +0800

    init

---
+++ import.py
@@ -0,0 +1,65 @@
+import http.client, urllib.parse
+import json
+import os
+
+import spotify_oauth_token
+
+uri = 'api.spotify.com'
+header = {
+    'Authorization': 'Bearer {}'.format(spotify_oauth_token.access_token),
+    'Content-Length': 0
+}
+
+folder = 'artist_list'
+
+artist_list = os.listdir(folder)
+print(artist_list)
+follow_limit = 50
+not_found_artist = []
+def concate_artist_id(batch_artist_id_follow):
+    batch_id = ''
+    for i in range(len(batch_artist_id_follow)):
+        batch_id += batch_artist_id_follow[i]
+        if i is not len(batch_artist_id_follow) - 1:
+            batch_id += ','
+    return batch_id
+def follow(batch_artist_id_follow):
+    conn = http.client.HTTPSConnection(uri)
+    batch_id = concate_artist_id(batch_artist_id_follow)
+    params = urllib.parse.urlencode({
+                    'type': 'artist',
+                    'ids': batch_id
+                })
+    query_str = '/v1/me/following?' + params
+    conn.request('PUT', query_str, None, header)
+    r1 = conn.getresponse()
+    print('batch follow response:', r1.status)
+
+for filename_list in artist_list:
+    with open(os.path.join(folder, filename_list), 'r') as f:
+        artists = f.readlines()
+    artists = [x.strip() for x in artists]
+    batch_artist_id_follow = []
+    for artist in artists:
+        print(artist)
+        # check artist
+        conn = http.client.HTTPSConnection(uri)
+        params = urllib.parse.urlencode({ 'q': artist, 'type': 'artist'})
+        query_str = '/v1/search?' + params
+        conn.request('GET', query_str, None, header)
+        r1 = conn.getresponse()
+        data = json.loads(r1.read())
+        if len(data['artists']['items']) == 0:
+            not_found_artist.append(artist)
+            continue
+        match_artist = data['artists']['items'][0]
+        batch_artist_id_follow.append(match_artist['id'])
+
+        # follow the artist
+        if len(batch_artist_id_follow) == follow_limit:
+            follow(batch_artist_id_follow)
+            batch_artist_id_follow = []
+    follow(batch_artist_id_follow)
+    break
+with open(os.path.join(folder, 'not_found_artist.txt'), 'w') as f:
+    json.dump(not_found_artist, f, ensure_ascii=False)
